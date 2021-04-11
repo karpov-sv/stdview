71db2109b8fab0aef70fd22fba0c20cc9e9f0f20
karpov-sv <karpov.sv@gmail.com>
2021-04-11 16:32:07 +0200

    Disable debug mode by default

--- stdview/cli.py
+++ stdview/cli.py
@@ -11,6 +11,8 @@ def main():
     parser.add_argument('-p', '--port', dest='port', type=int, default=5567, help='Port to use')
     parser.add_argument('-a', '--all', dest='show_all', action='store_true', help='Show all files')
 
+    parser.add_argument('--debug', dest='debug', action='store_true', help='Debug mode')
+
     parser.add_argument('-s', '--secret', dest='secret', type=str, default=os.urandom(24), help='Secret key')
 
     # TODO: option to listen for external requests
@@ -23,4 +25,4 @@ def main():
 
     app.secret_key = args.secret
 
-    app.run(port=args.port, debug=True)
+    app.run(port=args.port, debug=args.debug)
